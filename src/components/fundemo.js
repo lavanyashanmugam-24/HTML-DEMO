923abc335f5164ac8267e364bd526bdb87e42aa2
Lavanya A S <lavanyashanmugam24@gmail.com>
2022-04-23 18:38:38 +0530

    commit

---
+++ src/components/fundemo.js
@@ -0,0 +1,9 @@
+import React from "react";
+ function Fundemo (){
+     return(
+         <div>
+             <h1>Hello world from function</h1>
+         </div>
+     )
+ }
+ export default Fundemo;
